03763e5f0068c6edc6b121fa4a88ccb70b1a6fde
Igor Pontes <igorpontes8@gmail.com>
2023-02-24 16:06:51 -0300

    Git Search Done

---
+++ src/scripts/index.js
@@ -0,0 +1,12 @@
+import { getUserByName, getRepos } from "./request.js";
+
+export function showProfile () {
+    const input = document.querySelector('input');
+    const button = document.querySelector('button');
+
+    button.addEventListener('click', async() =>{
+        const repos = await getRepos(input.value);
+        const user = await getUserByName(input.value);
+    })
+}
+showProfile()
